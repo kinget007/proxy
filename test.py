1974cf553c6719f80ff3d6ad01dca2415a12bac2
king.yu <et007.king@gmail.com>
2018-10-08 17:11:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ class Counter:
                 "num": self.num,
                 "url": flow.request.pretty_url,
                 "method": str(flow.request.method),
-                "query": urllib.parse.unquote(str(flow.request.query).replace("\\x", "%"), encoding='utf-8',
+                "query": urllib.parse.unquote(str(flow.request.query).replace("\\x", "%").replace('MultiDictView', ''), encoding='utf-8',
                                               errors='replace'),
                 "post_data": urllib.parse.unquote(str(flow.request.raw_content).replace("\\x", "%"),
                                                   encoding='utf-8',
